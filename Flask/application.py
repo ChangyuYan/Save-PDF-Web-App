0cd26cb57c0626df34d47d74cb29e37e21a91262
Changyu Yan <changyannnnnn@gmail.com>
2020-11-11 13:50:37 -0800

    initial version ready; successfully perform main functions

--- Flask/application.py
+++ Flask/application.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap
-from werkzeug.utils import secure_filename
 from flask import send_file
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import requests
+from zipfile import ZipFile
+import os
 
 application = app = Flask(__name__)
 Bootstrap(app)
@@ -16,12 +21,39 @@ def index():
 def save_all_pdf():
     # Read from user input
     input_features = [x for x in request.form.values()]
-    url_entered = str.lower(str(input_features[0]))
-    print("Hello, world!")
-    print(url_entered)
-    print('------')
-    return render_template('index.html')
+    page_url = str.lower(str(input_features[0]))
+    u_client = uReq(page_url)
+    page_html = u_client.read()
+    u_client.close()
+
+    # Parse HTML
+    page_soup = soup(page_html, "html.parser")
+    # Grab each row
+    containers = page_soup.findAll("a")
+    link_prefix = page_url[: page_url.rfind('/')]
+
+    # Create a zipfile object
+    zip_file_name = 'pdf_files.zip'
+    zip_obj = ZipFile(zip_file_name, 'w')
+
+    for anchor in containers:
+        link = anchor.get('href')
+        if link and link[-3:] == 'pdf':
+            file_url = "%s/%s" % (link_prefix, link)
+            r = requests.get(file_url, stream=True)
+            current_file_name = ("%s.pdf" % anchor.text)
+            print(current_file_name)
+            with open(current_file_name, "wb") as pdf:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        pdf.write(chunk)
+            zip_obj.write(current_file_name)
+            os.remove(current_file_name)
+
+    zip_obj.close()
+
+    return send_file(zip_file_name)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
